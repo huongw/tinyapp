fd3446380c4be34381d9c24b3552f2f882d0b241
huongw <whitneyhuong@gmail.com>
2021-08-31 01:51:06 +0000

    implements post /urls

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
@@ -9,28 +10,33 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+function generateRandomString(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+ return result;
+};
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
 
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/new", (req, res) => {
+
+  res.render("urls_new");
+
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
 
-  const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
+  const templateVars = { shortURL, longURL };
   
   res.render("urls_show", templateVars);
 });
@@ -38,3 +44,12 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/")    
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
