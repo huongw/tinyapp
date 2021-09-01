06b52e5b013c6948d656c3a603b7249af5d7c5b0
huongw <whitneyhuong@gmail.com>
2021-09-01 23:23:58 +0000

    added function to check if email already exists inside user object and created if else statements for edge cases

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
@@ -13,7 +14,27 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString(length) {
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+const checkIfEmailExists = function(email, user) {
+  if (user.email === email) {
+    return true;
+  }
+  return false;
+}
+
+const generateRandomString = function(length) {
   var result = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
@@ -23,12 +44,18 @@ function generateRandomString(length) {
  return result;
 };
 
-// console.log(generateRandomString(6))
-
 app.get("/urls", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+
+  if (!user) {
+    res.send("You are not logged in")
+    return;
+  }
+
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user
   }
   res.render("urls_index", templateVars);
 })
@@ -36,18 +63,30 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL; // longURL is the name in the form, the variable longURL stores the value(website) from the body object
   const shortURL = generateRandomString(6);
-  console.log("shortURL", shortURL)
+
   urlDatabase[shortURL] = longURL; // stores the website into the urlDatabase object with the shortURL as the key
   res.redirect(`/urls/${shortURL}`);    
 });
 
 app.get("/urls/new", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+
+  if (!user) {
+    res.send("You are not logged in")
+    return;
+  }
+  
   const templateVars = {
-    username: req.cookies["username"]
+    user
   };
 
   res.render("urls_new", templateVars);
 })
+
+app.get("/register", (req, res) => {
+  res.render("registration");
+});
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -59,14 +98,48 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+
+  if (!user) {
+    res.send("You are not logged in")
+    return;
+  }
 
   const templateVars = {
     shortURL,
     longURL,
-    username: req.cookies["username"]
+    user
   }
 
   res.render("urls_show", templateVars);
+})
+
+app.post("/register", (req, res) => {
+  const userId = generateRandomString(6);
+  const email = req.body.email;
+  const password = req.body.password;
+  const newUser = {
+    id: userId,
+    email,
+    password
+  };
+  users[userId] = newUser;
+
+  const emailExists = checkIfEmailExists(email, users[userId])
+
+  if (users[userId].email === "" || users[userId].password === "") {
+    res.send("400");
+    return;
+  }
+  
+  if (emailExists) {
+    res.send("400");
+    return;
+  }
+
+  res.cookie("user_id", userId);
+  res.redirect("/urls");
 })
 
 app.post("/login", (req, res) => {
