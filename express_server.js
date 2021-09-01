9753632a0b3173fc8efc2f1ad33f1bb2d1fee712
huongw <whitneyhuong@gmail.com>
2021-09-01 18:03:32 +0000

    created post to delete resource and post to update link

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,50 +17,68 @@ function generateRandomString(length) {
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
-  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
  return result;
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+// console.log(generateRandomString(6))
 
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase}
   res.render("urls_index", templateVars);
+})
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL; // longURL is the name in the form, the variable longURL stores the value(website) from the body object
+  const shortURL = generateRandomString(6);
+  console.log("shortURL", shortURL)
+  urlDatabase[shortURL] = longURL; // stores the website into the urlDatabase object with the shortURL as the key
+  res.redirect(`/urls/${shortURL}`);    
 });
 
 app.get("/urls/new", (req, res) => {
-
   res.render("urls_new");
+})
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
 
-  const templateVars = { shortURL, longURL };
-  
+  const templateVars = {
+    shortURL,
+    longURL
+  }
+
   res.render("urls_show", templateVars);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL]; 
-  res.redirect(longURL);
-});
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls")
 })
 
-app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);  
+app.post("/urls/:shortURL/delete", (req, res) => {
+
+  const shortURL = req.params.shortURL;
+
+  delete urlDatabase[shortURL];
+
+  res.redirect("/urls");
+
 });
+
+app.post("/urls/:shortURL", (req, res) => {
+
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  
+  urlDatabase[shortURL] = longURL;
+
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
